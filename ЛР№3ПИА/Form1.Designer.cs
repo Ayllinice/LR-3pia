1d88b553a8e724f8c16d887d9a108d0c6b81c7f7
Ayllinice <m2209576@edu.misis.ru>
2023-04-01 23:00:27 +0300

    Добавьте файлы проекта.

---
+++ ЛР№3ПИА/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ЛР_3ПИА
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(276, 236);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+    }
+}
+
---
+++ ЛР№3ПИА/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ЛР_3ПИА
+{
+    public partial class Form1 : Form
+    {
+        public string player;
+        public Form1()
+        {
+            InitializeComponent();
+            player = "X";
+            panel11.BackColor= Color.Cyan;
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseEnter(object sender, EventArgs e)
+        {
+            Panel_chengecolor(sender, e);
+            
+        }
+        public void Panel_chengecolor(object sender, EventArgs e)
+        {
+            Panel panel = sender as Panel;
+            if (panel != null)
+            {
+                panel.BackColor = Color.LightGray; // задаем новый цвет фона
+            }
+        }
+        private void panel1_MouseLeave(object sender, EventArgs e)
+        {
+            Panel panel = sender as Panel;
+            if (panel != null)
+            {
+                panel.BackColor = Color.Transparent; // задаем новый цвет фона
+            }
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void label4_MouseEnter(object sender, EventArgs e) //перекрашиваем лейбл при наведении мышкой
+        {
+            Label label = sender as Label;
+            label.BackColor = Color.LightGray;
+        }
+
+        private void label4_MouseLeave(object sender, EventArgs e)
+        {
+            Label label = sender as Label;
+            label.BackColor = Color.Transparent;
+        }
+
+        private object label4_MouseClick(object sender, MouseEventArgs e)
+        {
+            switch (player)
+            {
+                case "X":
+                    panel11.BackColor= Color.Transparent;
+                    sender.GetType().GetProperty("Text").SetValue(sender, "O");
+                    player = "O";
+                    panel10.BackColor= Color.Cyan;
+                    if (CheckForWinner(player) == true)
+                    {
+                        MessageBox.Show("Player O Won");
+
+                    }
+                    break;
+
+
+                case "O":
+                    panel10.BackColor= Color.Transparent;
+                    sender.GetType().GetProperty("Text").SetValue(sender, "X");
+                    player = "X";
+                    panel11.BackColor = Color.Cyan;
+                    if (CheckForWinner(player) == true)
+                    {
+                        MessageBox.Show("Player X Won");
+                        
+                    }
+                    break;
+
+            
+            
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+            private void panel11_Paint(object sender, PaintEventArgs e)
+            {
+
+            }
+
+            private void Form1_Load(object sender, EventArgs e)
+            {
+
+            }
+
+            private void button2_Click(object sender, EventArgs e)
+            {
+                for (int i = 4; i <= 13; i++)
+                {
+                    Control[] controls = this.Controls.Find("label" + i.ToString(), true);
+                    if (controls.Length > 0)
+                    {
+                        Label label = (Label)controls[0];
+                        label.Text = "";
+                    }
+                }
+            }
+
+
+            private void Form1_Paint(object sender, PaintEventArgs e)
+            {
+
+            }
+            public bool CheckForWinner(string player)
+            {
+                string[,] gameBoard = new string[3, 3];
+                gameBoard[0, 0] = label4.Text;
+                gameBoard[0, 1] = label6.Text;
+                gameBoard[0, 2] = label7.Text;
+                gameBoard[1, 0] = label8.Text;
+                gameBoard[1, 1] = label9.Text;
+                gameBoard[1, 2] = label10.Text;
+                gameBoard[2, 0] = label11.Text;
+                gameBoard[2, 1] = label12.Text;
+                gameBoard[2, 2] = label13.Text;
+                // Проверяем по горизонтали
+                for (int row = 0; row < 3; row++)
+                {
+                    if (gameBoard[row, 0] == player.ToString() && gameBoard[row, 1] == player.ToString() && gameBoard[row, 2] == player.ToString())
+                    {
+                        return true;
+                    }
+                }
+
+                // Проверяем по вертикали
+                for (int col = 0; col < 3; col++)
+                {
+                    if (gameBoard[0, col] == player.ToString() && gameBoard[1, col] == player.ToString() && gameBoard[2, col] == player.ToString())
+                    {
+                        return true;
+                    }
+                }
+
+                // Проверяем по диагонали
+                if (gameBoard[0, 0] == player.ToString() && gameBoard[1, 1] == player.ToString() && gameBoard[2, 2] == player.ToString())
+                {
+                    return true;
+                }
+                if (gameBoard[0, 2] == player.ToString() && gameBoard[1, 1] == player.ToString() && gameBoard[2, 0] == player.ToString())
+                {
+                    return true;
+                }
+
+                return false;
+            }
+    }
+}
+
